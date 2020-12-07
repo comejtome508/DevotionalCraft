73f8fbe8760db8c81fe2600ed05330b123c16f65
comejtome508 <youngnoh.goh@gmail.com>
2020-12-07 19:06:31 +0900

    Add router module

--- angular-devotional-craft/src/app/app.module.ts
+++ angular-devotional-craft/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { SettingWordsComponent } from './setting-words/setting-words.component';
-import { ArtworksListComponent } from './artworks-list/artworks-list.component';
+import { SettingWordsComponent } from './home/setting-words/setting-words.component';
+import { ArtworksListComponent } from './home/artworks-list/artworks-list.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
+import { HomeComponent } from './home/home.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule,Routes} from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
+const router : Routes = [
+  {path : 'artworks-list', component : ArtworksListComponent},
+  {path : 'card-detail', component : CardDetailComponent},
+  {path : 'home', component : HomeComponent},
+  {path : '', redirectTo : '/home', pathMatch : 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -18,9 +31,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     NavigationComponent,
     SettingWordsComponent,
     ArtworksListComponent,
+    CardDetailComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(router,{enableTracing:false}),
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
