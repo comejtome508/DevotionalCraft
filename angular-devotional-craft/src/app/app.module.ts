9ecf1de1ac26f90876d3906b3f8ad8354ecb588e
comejtome508 <youngnoh.goh@gmail.com>
2020-12-07 17:43:11 +0900

    Put angular material for main page

--- angular-devotional-craft/src/app/app.module.ts
+++ angular-devotional-craft/src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SettingWordsComponent } from './setting-words/setting-words.component';
 import { ArtworksListComponent } from './artworks-list/artworks-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     SettingWordsComponent,
-    ArtworksListComponent
+    ArtworksListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
